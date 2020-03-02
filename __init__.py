fcc7743ae5152adbf66d833a2d45cbf0c6657ad7
Joel Berger <joel.a.berger@gmail.com>
2020-03-02 15:19:37 +0000

    update for common play skill

--- __init__.py
+++ __init__.py
@@ -1,30 +1,44 @@
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.core import intent_handler
 from mycroft.util.log import LOG
 from mycroft.skills.audioservice import AudioService
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 import requests
 import html
 import re
 
-class WFMTSkill(MycroftSkill):
+class WFMTSkill(CommonPlaySkill):
 
-    def __init__(self, name=None):
-        super(WFMTSkill, self).__init__(name="WFMTSkill")
+    def CPS_match_query_phrase(self, phrase):
+        """ This method responds wether the skill can play the input phrase.
 
-    def initialize(self):
-        self.log.info('initializing Audio Service')
-        self.audio_service = AudioService(self.emitter)
+            The method is invoked by the PlayBackControlSkill.
 
-    @intent_handler(IntentBuilder("PlayIntent").require("Play"))
-    def handle_play_intent(self, message):
+            Returns: tuple (matched phrase(str),
+                            match level(CPSMatchLevel),
+                            optional data(dict))
+                     or None if no match was found.
+        """
+        if (phrase.lower() == 'wfmt'):
+                return (phrase, CPSMatchLevel.EXACT)
+        return None
+
+    def CPS_start(self, phrase, data):
+        """ Starts playback.
+
+            Called by the playback control skill to start playback if the
+            skill is selected (has the best match level)
+        """
+        if self.audioservice.is_playing:
+            self.log.info('stopping existing stream')
+            self.audioservice.stop()
         self.log.info('starting WFMT stream')
-        self.audio_service.stop()
-        self.audio_service.play('http://stream.wfmt.com/main-mp3')
+        self.audioservice.play(['https://wfmt.streamguys1.com/main-mp3'], 'vlc')
 
     def stop(self):
-        if self.audio_service.is_playing:
+        if self.audioservice.is_playing:
             self.log.info('stopping WFMT stream')
-            self.audio_service.stop()
+            self.audioservice.stop()
             return True
         else:
             return False
